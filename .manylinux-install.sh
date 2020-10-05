e9680bc49f7d7543106566ba9c1083841b294ced
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-10-05 15:51:35 +0530

    Update .manylinux-install.sh

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -9,23 +9,15 @@ for PYBIN in /opt/python/*/bin; do
        [[ "${PYBIN}" == *"cp36"* ]] || \
        [[ "${PYBIN}" == *"cp37"* ]] || \
        [[ "${PYBIN}" == *"cp38"* ]]; then
+        "${PYBIN}/pip" install -e /io/
+        "${PYBIN}/pip" wheel /io/ -w wheelhouse/
         if [ `uname -m` == 'aarch64' ]; then
-         "${PYBIN}/pip" install virtualenv
-         "${PYBIN}/python" -m virtualenv .venv
-         source .venv/bin/activate
-         pip install -e /io/
-         pip wheel /io/ -w wheelhouse/
          pip install tox
          cd /io/
          tox -e $toxenv py`echo "${PYBIN}" | cut -f 4 -d"/" | cut -f 1 -d"-" | cut -c3-`
          cd ..
-         deactivate
-         rm -rf .venv
-        else
-         "${PYBIN}/pip" install -e /io/
-         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-         rm -rf /io/build /io/*.egg-info
-        fi  
+        fi
+        rm -rf /io/build /io/*.egg-info
     fi
 done
 
